c30eb0537d542857d245771422e257842c339619
Joe Webber <35155442+j-webber@users.noreply.github.com>
2025-04-30 12:35:38 -0400

    cleaned up and shorted merge_sort

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,30 +1,22 @@
-def merge_sort(arr)
-  if arr.length < 2
-    return arr
+def merge_sort(array)
+  
+  return array if array.length <= 1
+
+  middle = array.length / 2
+
+  left_half = merge_sort(array[0, middle])
+  right_half = merge_sort(array[middle..-1])
+
+  merged_array = []
+
+  until left_half.empty? || right_half.empty? do
+    left_half.first < right_half.first ?  merged_array << left_half.shift : merged_array << right_half.shift
   end
 
-  left_half = merge_sort(arr.slice!(0, arr.length / 2))
-  right_half = merge_sort(arr)
-
-  puts "left half[0]: #{left_half[0]}"
-  puts "right half[0]: #{right_half[0]}"
-
-  sorted_arr = []
-
-  until left_half.length == 0 && right_half.length == 0 do
-    if left_half.length == 0 
-      sorted_arr.push(right_half.slice!(0))
-    elsif right_half.length == 0 || left_half[0] < right_half[0]
-      sorted_arr.push(left_half.slice!(0))
-    else 
-      sorted_arr.push(right_half.slice!(0))
-    end
-  end
-  puts "sorted array: #{sorted_arr}"
-  return sorted_arr
+merged_array + left_half + right_half
 end
 
-print merge_sort([])
+print merge_sort([3, 2, 1, 13, 8, 5, 0])
 
 
 
